4d0b7211d66d2dec2b4ac6ea78b7b150ae369880
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-08-27 22:00:39 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 
-fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+fn main() -> io::Result<()> {
+    let mut buf = [0; 1024];
+    let mut stdin = io::stdin();
+    loop {
+        print!("$ ");
+        let len = stdin.read(&mut buf)?;
+        let command = String::from_utf8(buf[..len].to_vec()).unwrap();
+        print!("{command}: command not found");
+        io::stdout().flush().unwrap();
+    }
 }
